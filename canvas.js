2edcf2fe49e62e58966f9a8541100128d47e16ea
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-12-05 14:22:51 +0100

    fix: canvas

--- canvas.js
+++ canvas.js
@@ -1,4 +1,11 @@
-import { createCanvas } from "canvas";
+import { createCanvas, GlobalFonts } from "@napi-rs/canvas";
+
+function registerFont(path, options) {
+    GlobalFonts.registerFromPath(path, options.family);
+}
+
+registerFont("Arial.ttf", { family: "Arial" });
+registerFont("Roboto-Regular.ttf", { family: "Roboto" });
 
 export function getTextWidthInPixels(text, fontFamily, sizeInPixels) {
     const canvas = createCanvas(200, 50);
--- example.js
+++ example.js
@@ -2,6 +2,7 @@ import { getTextWidthInPixels } from './canvas.js'
 
 // Exemple d'utilisation
 const text = "Découvrez les meilleurs outils SEO pour booster votre visibilité";
-const width = getTextWidthInPixels(text, 'Arial', 16);
-
-console.log(`Largeur du texte : ${width} pixels`);
+let width = getTextWidthInPixels(text, 'Arial', 16);
+console.log(`Largeur du texte (Arial): ${width} pixels`);
+width = getTextWidthInPixels(text, 'Roboto', 16);
+console.log(`Largeur du texte (Roboto): ${width} pixels`);
