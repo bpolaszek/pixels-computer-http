4b2bf3c67f99777b9fb7e47fcb0edec670062470
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-12-05 13:52:31 +0100

    feat: measure

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const {getTextWidthInPixels} = require("../../canvas")
 
 const app = express();
+app.use(express.json());
+
+const measureText = (text, fontFamily, sizeInPixels) => {
+  if (!text || !fontFamily || !sizeInPixels) {
+    throw new Error('Missing required parameters. Please provide text, fontFamily and sizeInPixels');
+  }
+  return getTextWidthInPixels(text, fontFamily, parseInt(sizeInPixels));
+};
 
 // Gérer la route racine de la fonction serverless
 app.get('/.netlify/functions/server', (req, res) => {
-  res.status(200).json({ message: 'Hello world from /.netlify/functions/server' });
+  //res.status(200).json({ message: 'Hello world from /.netlify/functions/server' });
+  const { text, fontFamily, sizeInPixels } = req.query;
+
+  try {
+    const width = measureText(text, fontFamily, sizeInPixels);
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=87400');
+    res.json({ width });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // Gérer la route racine du site
